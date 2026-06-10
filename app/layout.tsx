48d7e5cb2134a83f4695559596894f2cc87bc11b
이현규 <lhyunkyu2008@gmail.com>
2026-06-10 17:09:43 +0900

    feat :: 레이아웃 타이틀 변경 및 독자적인 파비콘 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,24 +12,11 @@ const doHyeon = Do_Hyeon({
 });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Reloop',
+  description: 'sdgs 목표 12번',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
+    icon: '/Reloop_icon.svg',
     apple: '/apple-icon.png',
   },
 }
